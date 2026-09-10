68ce1e86059fc0feb664a54e24004375b6a7bd06
North-West-Wind <wsyn148@gmail.com>
2026-09-10 11:06:16 +0800

    refactor: arguments of provider interface

--- datbox-core.go
+++ datbox-core.go
@@ -13,13 +13,13 @@ type ProviderInfo struct {
 }
 
 type Provider interface {
-	Get(path string, writer io.Writer) error // Get a file at the path and write to the writer
-	Put(path string, reader io.Reader) error // Put a file to the path using the reader
+	Get(path string, writer io.WriteCloser) error            // Get a file at the path and write to the writer
+	Put(path string, size int64, reader io.ReadCloser) error // Put a file to the path using the reader
 
 	List(path string) ([]fs.DirEntry, error)          // List files under a directory
 	Move(src, dst string) error                       // Move file from src to dst
 	Delete(path string, recursive, remote bool) error // Delete file at the path, optionally recursively and remotely
-	Mkdir(path string, all bool) error                // Create a directory, optionally create parents if they don't exist
+	Mkdir(path string, parents bool) error            // Create a directory, optionally create parents if they don't exist
 	Copy(src, dst string) error                       // Copy file (reference) from src to dst
 
 	Info() (ProviderInfo, error) // Fetch info of the provider
